dd93be7e75dc9644903462748317ae04c8e59369
Richard Flamsholt <richard.flamsholt@telerik.com>
2014-12-15 11:45:07 +0100

    Updated to latest iOS lib

--- lib/ios/EQATECAnalyticsMonitor.h
+++ lib/ios/EQATECAnalyticsMonitor.h
@@ -26,7 +26,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-#define EQATEC_ANALYTICS_MONITOR_VERSION "3.2.111"
+#define EQATEC_ANALYTICS_MONITOR_VERSION "3.2.123"
 
 // forward declarations
 @protocol EQATECLogAnalyticsMonitor;
